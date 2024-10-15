a017d09e7b915630ff6ac64ab4411153ebaef761
QuangCoDon <quangluuyenho5b@gmail.com>
2024-10-15 15:30:16 +0700

    BeginHomePageRender

--- src/component/HomeCard.js
+++ src/component/HomeCard.js
@@ -1,14 +1,24 @@
 import React from 'react'
 
-const HomeCard = ({name, image, category, price}) => {
+const HomeCard = ({name, image, category, price, loading}) => {
   return (
-    <div className='bg-white shadow-md p-2 rounded'>
-        <div className='w-40 min-h-[150px]'>
+    <div className='bg-white shadow-md p-2 rounded min-w-[150px]'>
+       {
+        name ? (<>
+         <div className='w-40 min-h-[150px]'>
             <img src={image} className='h-full w-full'/>
-        </div>
+          </div>
         <h3 className='font-semibold text-slate-600 text-center capitalize text-lg'>{name}</h3>
         <p className='text-center text-slate-500 font-medium'>{category}</p>
-        <p className='text-center font-bold'><span className='text-red-500'>₹</span><span>{price}</span></p>
+        <p className='text-center font-bold'><span className='text-red-500'>$</span><span>{price}</span></p>
+        </>
+       )
+       : (
+      <div className='flex justify-center items-center h-full min-h-[150px] w-40'>
+        <p>{loading}</p>
+      </div>
+        )
+      }
     </div>
   )
 }
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import HomeCard from '../component/HomeCard'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import HomeCard from '../component/HomeCard';
+import { useSelector } from 'react-redux';
+import CardFeature from '../component/CardFeature';
+import {GrPrevious}  from'react-icons/gr';
+import {GrNext}  from'react-icons/gr';
 
 const Home = () => {
   const productData = useSelector((state)=>state.product.productList);
-  const homeProductCartList = productData.slice(0,6)
+  const homeProductCartList = productData.slice(1,5)
+  const homeProductCartListVegetables = productData.filter(e1 => e1.category === 'vegetable', [])
+  console.log(homeProductCartListVegetables)
+  
+  const loadingArray = new Array(3).fill(null)
   return (
     <div className='p-2 md:p-4'>
       <div className='md:flex gap-4 py-2'>
@@ -14,7 +21,7 @@ const Home = () => {
             <p className='text-sm font-medium text-slate-900'>Bike Delivery</p>
             <img src='https://cdn-icons-png.flaticon.com/512/2972/2972185.png' className='h-7'/>
           </div>
-          <h2 className='text-4xl md:text-7xl font-bold py-3'> The Fasted Delivery in <span className='text-red-600'>Your Home</span></h2>
+          <h2 className='text-4xl md:text-7xl font-bold py-3'> The Fasted Delivery in <span className='text-red-600 text-'>Your Home</span></h2>
           <p className='py-3 text-base'>Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
@@ -25,24 +32,54 @@ const Home = () => {
 
         <div className='md:w-1/2 flex flex-wrap gap-5 p-4 justify-center'>
         {
-          homeProductCartList[0] && homeProductCartList.map((e1)=>{
+          homeProductCartList[0] ? homeProductCartList.map((e1)=>{
             return(
             <HomeCard
               key={e1._id}
+              id={e1._id}
               image={e1.image}
               name={e1.name}
               price={e1.price}
               category={e1.category}
             />
+            );
+          })
+          : 
+          loadingArray.map((e1, index)=>{
+            return (
+              <HomeCard
+                key = {index}
+                loading={"Loading..."}
+              />
             )
           })
         }
         </div>
+      </div>
 
-        <div className = "">
-
+      <div className = "">
+        <div>
+          <h2 className='font-bold text-2xl text-slate-800 mb-4'>Fresh Vegetables</h2>
         </div>
-
+        <div className=''>
+          <button><GrPrevious/></button>
+          <button><GrNext/></button>
+        </div>
+        <div className='flex gap-5 overflow-scroll'>
+          {
+            homeProductCartListVegetables.map(e1=>{
+              return(
+                <CardFeature
+                  key = {e1._id}
+                  name = {e1.name}
+                  category = {e1.category}
+                  price = {e1.price}
+                  image = {e1.image}
+                />
+              )
+            })
+          }
+        </div>
       </div>
     </div>
   )
